2d2e74ae446837363602aa9745a412d5e4048c83
Anderson Andrade <anderson_andrade_@Outlook.com>
2024-04-14 10:31:39 -0300

    refact: change build configuration to remove unsed images when build a new image

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Remove the previous Docker image (if exists)
-docker image rm -f personal_saas_server_personal_server:latest
+docker image rm -f personal_saas_server-personal_server:latest
 
 # Build the new Docker image
 docker-compose build
 
 # Start the Docker containers
 docker-compose up -d
+
+# Remove dangling images
+docker image prune -f
