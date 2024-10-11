b2a84c816f0cebd17d3c24f924e828031a046a11
Anderson Andrade <anderson_andrade_@Outlook.com>
2024-10-10 23:58:28 -0300

    refact: save to run eslint

--- src/infra/swagger/swagger.config.ts
+++ src/infra/swagger/swagger.config.ts
@@ -3,12 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
 
 class SwaggerConfig {
-  private config = new DocumentBuilder()
-    .setTitle('Personal SaaS')
-    .setDescription('')
-    .setVersion('0.1')
-    .addTag('')
-    .build();
+  private config = new DocumentBuilder().setTitle('Personal SaaS').setDescription('').setVersion('0.1').addTag('').build();
 
   initSwagger(application: INestApplication): void {
     const document = SwaggerModule.createDocument(application, this.config);
