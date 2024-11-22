f49a93772622dcd4f2b93ae3e396eec2fd45d6c1
Anderson Andrade <anderson_andrade_@Outlook.com>
2024-11-22 00:07:40 -0300

    refact: implement port in the env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import swaggerConfig from './infra/swagger/swagger.config';
-
-import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import swaggerConfig from './infra/swagger/swagger.config';
 
 async function bootstrap() {
   const logger = new Logger('InstanceLoader');
@@ -13,12 +12,12 @@ async function bootstrap() {
 
   const port = configService.get<number>('port') || 3000;
 
-  logger.debug(`Application run in port ${port}`);
-
   swaggerConfig.initSwagger(app);
 
   app.enableCors();
 
   await app.listen(port);
+
+  logger.debug(`Application run in port ${port}`);
 }
 bootstrap();
